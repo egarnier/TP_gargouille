c69cc044c2c55c23cf6aca22f56fac42e11b1c58
Garnier Elodie <egarnier@bs406-s16-03.insa-lyon.fr>
2014-11-20 18:29:24 +0100

    Simplified the main
    It now just creates some pictures with differents constructors and applied the functions on it.

--- ppm.cpp
+++ ppm.cpp
@@ -14,9 +14,8 @@ int main(int argc, char* argv[])
   //--------------------------------------------------------------------------
   // Read file "gargouille.ppm" into image (width and height)
   //--------------------------------------------------------------------------
-  picture* gargouille;
-  gargouille=(picture*)malloc(sizeof(gargouille));
-  ppm_read_from_file(gargouille, "gargouille.ppm");
+  picture gargouille;
+  gargouille.ppm_read_from_file("gargouille.ppm");
 
 
   //--------------------------------------------------------------------------
@@ -24,46 +23,33 @@ int main(int argc, char* argv[])
   // write it into "gargouille_BW.ppm"
   //--------------------------------------------------------------------------
   // Creation and copy image into image_bw
-  picture* gargouille_bw;
-  gargouille_bw=(picture*)malloc(sizeof(gargouille_bw));
-  gargouille_bw->width  = gargouille->width;
-  gargouille_bw->height = gargouille->height;
-  gargouille_bw->data = (u_char*) malloc(3 * gargouille->width * gargouille->height * sizeof(*(gargouille_bw->data)));
-  memcpy(gargouille_bw->data, gargouille->data, 3 * gargouille_bw->width * gargouille_bw->height * sizeof(*(gargouille_bw->data)));
+  picture gargouille_bw = picture(gargouille);
 
   // Desaturate image_bw
-  ppm_desaturate(gargouille_bw);
+  gargouille_bw.ppm_desaturate();
 
   // Write the desaturated image into "gargouille_BW.ppm"
-  ppm_write_to_file(gargouille_bw,"gargouille_BW.ppm");
+  gargouille_bw.ppm_write_to_file("gargouille_BW.ppm");
 
-  // Free the desaturated image
-  free(gargouille_bw->data);
-  free(gargouille_bw);
 
   //--------------------------------------------------------------------------
   // Create a resized copy of the image and
   // write it into "gargouille_small.ppm"
   //--------------------------------------------------------------------------
   // Creation and copy image into image_small
-  picture* gargouille_small;
-  gargouille_small=(picture*)malloc(sizeof(gargouille_small));
-  gargouille_small->width  = gargouille->width;
-  gargouille_small->height = gargouille->height;
-  gargouille_small->data = (u_char*) malloc(3 * gargouille->width * gargouille->height * sizeof(*(gargouille_small->data)));
-  memcpy(gargouille_small->data, gargouille->data, 3 * gargouille_small->width * gargouille_small->height * sizeof(*(gargouille_small->data)));
+  picture gargouille_small = picture(gargouille);
 
   // Shrink image_small size 2-fold
-  ppm_shrink(gargouille_small, 2);
+  gargouille_small.ppm_shrink(2);
 
   // Write the desaturated image into "gargouille_small.ppm"
-  ppm_write_to_file(gargouille_small, "gargouille_small.ppm");
+  gargouille_small.ppm_write_to_file("gargouille_small.ppm");
 
-  // Free the not yet freed images
-  free(gargouille_small->data);
-  free(gargouille_small);
-  free(gargouille->data);
-  free(gargouille);
+
+  // Delete the picture
+  delete(gargouille.GetData());
+  delete(gargouille_bw.GetData());
+  delete(gargouille_small.GetData());
 
   return 0;
 }
